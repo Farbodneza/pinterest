e45fe10b67d63bfcf2f47e50f0d8cf078ed6a791
Farbod Nezafati <farbodneza@gmail.com>
2025-06-26 16:33:52 +0330

    add(pins.models):pin model

--- pins/models.py
+++ pins/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from taggit.managers import TaggableManager
+# Upload Pin (Image, title, description, tags)
 # Create your models here.
+class Pin(models.Model):
+    image = models.ImageField(upload_to = "media/images")
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    tags = TaggableManager()
+
+
+class Wishlist(models.Model):
+
+# Wishlist (Save pins to a "Saved for Later" list)
