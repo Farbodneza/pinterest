9eda7c1e4da301d90e89bbac71ba7ac7c6ffb7fa
Mohammad ubuntu <mmdvafaj@gmail.com>
2025-06-26 15:32:16 +0330

    add User model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class CustomUser(User):
+    profile = models.ImageField(upload_to='profiles', null=True, blank=True)
+    bio = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.username
